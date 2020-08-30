3cb1c9937fa8aa2b4d1f75f6469b5bd1219c125e
FrancoAdN <francoadinapoli@gmail.com>
2020-08-30 18:35:44 -0300

    add

---
+++ public/build/precache-manifest.453902ccd5e8e0554d2c507145f57a6f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315889d7cfaaaf5e9b3edcda77911bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d611b31f59b95fdf3bf6",
+    "url": "/static/css/main.ef2a7e64.chunk.css"
+  },
+  {
+    "revision": "b177b20c8754c833b73e",
+    "url": "/static/js/2.d9624eb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d9624eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d611b31f59b95fdf3bf6",
+    "url": "/static/js/main.02d00b84.chunk.js"
+  },
+  {
+    "revision": "51b7939be817c75de97a",
+    "url": "/static/js/runtime-main.8e517ac9.js"
+  },
+  {
+    "revision": "092bfa54aad74ce9ab2de010031731f5",
+    "url": "/static/media/1.092bfa54.png"
+  },
+  {
+    "revision": "7d104e2e8f9d0e45e17a527a66b11c15",
+    "url": "/static/media/2.7d104e2e.png"
+  },
+  {
+    "revision": "a3c00a49e7805f6be60c84ea3475e596",
+    "url": "/static/media/3.a3c00a49.png"
+  },
+  {
+    "revision": "f8073a8ba059511a996b1446f380f78c",
+    "url": "/static/media/4.f8073a8b.png"
+  },
+  {
+    "revision": "637addf55b1153f369662a1de0491aec",
+    "url": "/static/media/5.637addf5.png"
+  },
+  {
+    "revision": "6e03ace1b7d803c56d5fb1524f01dab8",
+    "url": "/static/media/6.6e03ace1.png"
+  },
+  {
+    "revision": "c8a6e9c71f02dfdae1b2ba0ac08fbe40",
+    "url": "/static/media/7.c8a6e9c7.png"
+  },
+  {
+    "revision": "d107448c656a1ba203191a238bebe8dd",
+    "url": "/static/media/8.d107448c.png"
+  },
+  {
+    "revision": "6b7d48961fd3c0cd42a36cd472db7fd5",
+    "url": "/static/media/9.6b7d4896.png"
+  },
+  {
+    "revision": "d2951ceb6fb732384db52b3b80e3772f",
+    "url": "/static/media/Omen.d2951ceb.gif"
+  },
+  {
+    "revision": "bbb38d800d9c4964dc7899823f5f2e0c",
+    "url": "/static/media/Reyna.bbb38d80.gif"
+  },
+  {
+    "revision": "e68923c93d2cf01d2fee5418a6529128",
+    "url": "/static/media/Sage.e68923c9.gif"
+  },
+  {
+    "revision": "139e74e298bca37a25d2bd5868552e04",
+    "url": "/static/media/fontawesome-webfont.139e74e2.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  },
+  {
+    "revision": "eb4e47867b0327273c095c524dcaace4",
+    "url": "/static/media/logo-rec.eb4e4786.png"
+  },
+  {
+    "revision": "cbf05b62d7b286c724718fdf914953a1",
+    "url": "/static/media/payments.cbf05b62.png"
+  },
+  {
+    "revision": "6bc3171f15c4f81d25b49797f380ba16",
+    "url": "/static/media/payments2.6bc3171f.png"
+  },
+  {
+    "revision": "ec48487c4a462fefe79ebc4c8532faa8",
+    "url": "/static/media/ppp.ec48487c.png"
+  },
+  {
+    "revision": "156fc63efe16c3c6c48eedd8dfa07524",
+    "url": "/static/media/test.156fc63e.webm"
+  },
+  {
+    "revision": "6eb58c98e7243e715beaa23497ce723d",
+    "url": "/static/media/test10.6eb58c98.gif"
+  },
+  {
+    "revision": "3b962f5bd596869353e3fbfa68293ebb",
+    "url": "/static/media/test112.3b962f5b.jpg"
+  },
+  {
+    "revision": "0a77b402279853a41a1e0be557d22b37",
+    "url": "/static/media/test3.0a77b402.jpg"
+  },
+  {
+    "revision": "153c8eedccdc98658828a671253314f4",
+    "url": "/static/media/test3.153c8eed.gif"
+  },
+  {
+    "revision": "2a209124200e7def1bc82f7ba6e6b07e",
+    "url": "/static/media/test4.2a209124.gif"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,49 @@
+/*
+// SDK de Mercado Pago
+const mercadopago = require('mercadopago');
+
+// Agrega credenciales
+mercadopago.configure({
+    access_token: 'PROD_ACCESS_TOKEN'
+});
+
+// Crea un objeto de preferencia
+let preference = {
+    items: [
+        {
+            title: 'Mi producto',
+            unit_price: 100,
+            quantity: 1,
+        }
+    ]
+};
+
+mercadopago.preferences.create(preference)
+    .then(function (response) {
+        // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+        global.id = response.body.id;
+    }).catch(function (error) {
+        console.log(error);
+    });
+*/
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
+})
+
+app.get('/boost', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
+})
+
+app.get('/*', (req, res) => {
+    res.redirect('/')
+});
+
+app.listen(PORT, () => console.log('Server running. Port: ' + PORT));
